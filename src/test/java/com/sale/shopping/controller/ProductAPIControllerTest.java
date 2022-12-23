f5d753394b3ef609e23c37b153b76013a11f198a
KYUNGMIN <nkm4095@naver.com>
2022-12-23 16:44:22 +0900

    ProductAPIController(RestController)  Junit Insert Test

--- src/test/java/com/sale/shopping/controller/ProductAPIControllerTest.java
+++ src/test/java/com/sale/shopping/controller/ProductAPIControllerTest.java
@@ -1,19 +1,11 @@
 package com.sale.shopping.controller;
 
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.jayway.jsonpath.DocumentContext;
-import com.jayway.jsonpath.JsonPath;
-import com.mysql.cj.log.LogFactory;
 import com.sale.shopping.controller.api.ProductAPIController;
 import com.sale.shopping.model.dto.ProductRequestDTO;
 import com.sale.shopping.model.dto.ProductResponseDTO;
-import com.sale.shopping.model.entity.Product;
-import com.sale.shopping.repository.ProductRepository;
-import com.sale.shopping.repository.ProductRepositoryTest;
 import com.sale.shopping.service.ProductServiceImpl;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
@@ -21,9 +13,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.*;
-import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -36,9 +26,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(ProductAPIController.class)
-public class ProductControllerTest {
+public class ProductAPIControllerTest {
 
-    private final Logger log = LoggerFactory.getLogger(ProductControllerTest.class);
+    private final Logger log = LoggerFactory.getLogger(ProductAPIControllerTest.class);
 
     @Autowired
     private MockMvc mockMvc;
@@ -54,32 +44,6 @@ public class ProductControllerTest {
     @Test
     @DisplayName("컨트롤러 상품 등록 테스트 ")
     public void insertTest() throws Exception {
-     /*   // given
-        ProductRequestDTO productRequestDTO = ProductRequestDTO.builder()
-                .productTitle("제목")
-                .productImage("이미지주소")
-                .productContent("내용")
-                .productCount(1000)
-                .productPrice(987654321)
-                .build();
-
-        String body = objectMapper.writeValueAsString(productRequestDTO);
-
-        // when
-        HttpEntity<String> request = new HttpEntity<>(body, headers);
-
-        ResponseEntity<String> response = testRestTemplate.exchange("/api/product", HttpMethod.POST, request, String.class);
-
-
-        // then
-        DocumentContext dc = JsonPath.parse(response.getBody());
-
-        String title = dc.read("$.data.productTitle");
-        String author = dc.read("$.data.productImage");
-
-        log.info("============================================");
-        log.info(title);
-        log.info(author);*/
 
 
         given(productService.insertProduct(ProductRequestDTO.builder()
