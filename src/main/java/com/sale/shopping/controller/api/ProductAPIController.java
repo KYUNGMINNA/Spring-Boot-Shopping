4804349ae86a5b764b001cd8dac1812e17f8fa0f
KYUNGMIN <nkm4095@naver.com>
2022-12-23 20:10:39 +0900

    ProductAPIController(RestController)  - Restful

--- src/main/java/com/sale/shopping/controller/api/ProductAPIController.java
+++ src/main/java/com/sale/shopping/controller/api/ProductAPIController.java
@@ -3,17 +3,17 @@ package com.sale.shopping.controller.api;
 import com.sale.shopping.model.dto.CommonDTO;
 import com.sale.shopping.model.dto.ProductRequestDTO;
 import com.sale.shopping.model.dto.ProductResponseDTO;
+import com.sale.shopping.model.entity.Product;
+import com.sale.shopping.repository.ProductRepository;
 import com.sale.shopping.service.ProductServiceImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -29,5 +29,32 @@ public class ProductAPIController {
         return new ResponseEntity<>(CommonDTO.builder().statusCode(HttpStatus.OK.value()).data(productResponseDTO).build(),HttpStatus.CREATED);
     }
 
+    @GetMapping("/product")
+    public ResponseEntity<Object> SelectAllProduct(){
+        List<ProductResponseDTO> productResponseDTOList=productService.selectAllProduct();
+        return new ResponseEntity<>(CommonDTO.builder().statusCode(HttpStatus.OK.value()).data(productResponseDTOList).build(),HttpStatus.OK);
+
+    }
+
+    @GetMapping("/product/{id}")
+    public ResponseEntity<Object> SelectOneProduct(@PathVariable Integer id){
+        ProductResponseDTO productResponseDTO =productService.selectOneProduct(id);
+        return new ResponseEntity<>(CommonDTO.builder().statusCode(HttpStatus.OK.value()).data(productResponseDTO).build(),HttpStatus.OK);
+
+    }
+
+    @DeleteMapping("/product/{id}")
+    public ResponseEntity<Object> deleteProduct(@PathVariable Integer id){
+        productService.deleteProduct(id);
+        return new ResponseEntity<>(CommonDTO.builder().statusCode(HttpStatus.OK.value()).data(null).build(),HttpStatus.OK);
+
+    }
+
+    @PutMapping("/product/{id}")
+    public ResponseEntity<Object> modifyProduct(@PathVariable Integer id,@RequestBody ProductRequestDTO productRequestDTO) {
+        ProductResponseDTO productResponseDTO = productService.modifyProduct(id, productRequestDTO);
+        return new ResponseEntity<>(CommonDTO.builder().statusCode(HttpStatus.OK.value()).data(productResponseDTO).build(),HttpStatus.OK);
+
+    }
 
 }
