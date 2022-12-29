c165b543575d0317f2f10e04ad0c8bae0e3cb06e
KYUNGMIN <nkm4095@naver.com>
2022-12-29 11:04:17 +0900

    ProductOrderRepository  JPA

--- src/main/java/com/sale/shopping/repository/ProductRepository.java
+++ src/main/java/com/sale/shopping/repository/ProductRepository.java
@@ -2,7 +2,15 @@ package com.sale.shopping.repository;
 
 import com.sale.shopping.model.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
 public interface ProductRepository  extends JpaRepository<Product,Integer> {
+
+
+    Optional<Product> findByProductTitle(String productTitle);
+
+    void deleteByProductTitle(String productTitle);
 
 
 }
