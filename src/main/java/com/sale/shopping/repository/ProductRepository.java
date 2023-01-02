8fea41b718a32b9a382cc64841d65bce141c3b86
KYUNGMIN <nkm4095@naver.com>
2023-01-02 23:24:54 +0900

    ProductRepository update

--- src/main/java/com/sale/shopping/repository/ProductRepository.java
+++ src/main/java/com/sale/shopping/repository/ProductRepository.java
@@ -1,8 +1,12 @@
 package com.sale.shopping.repository;
 
+import com.sale.shopping.model.dto.ProductResponseDTO;
 import com.sale.shopping.model.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ProductRepository  extends JpaRepository<Product,Integer> {
@@ -13,4 +17,12 @@ public interface ProductRepository  extends JpaRepository<Product,Integer> {
     void deleteByProductTitle(String productTitle);
 
 
+    Page<Product> findAllByProductCategory(String productCategory, Pageable pageable);
+
+
+    Optional<Product> findByProductCategoryAndProductTitle(String productCategory,String productTitle);
+
+
+
+    List<?> findTop4ByProductCategoryOrderByIdDesc(String productCategory);
 }
