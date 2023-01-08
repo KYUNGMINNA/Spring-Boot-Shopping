0a4a00b2d451502c85621e96bd940140d270fd63
KYUNGMIN <nkm4095@naver.com>
2023-01-08 21:52:02 +0900

    ProductService Interface Implements

--- src/main/java/com/sale/shopping/service/ProductServiceImpl.java
+++ src/main/java/com/sale/shopping/service/ProductServiceImpl.java
@@ -5,12 +5,13 @@ import com.sale.shopping.model.dto.ProductResponseDTO;
 import com.sale.shopping.model.entity.Product;
 import com.sale.shopping.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -33,16 +34,19 @@ public class ProductServiceImpl  implements ProductService{
 
     @Transactional(rollbackFor = RuntimeException.class)
     @Override
-    public ProductResponseDTO selectOneProduct(Integer id) {
-        return productRepository.findById(id).get().toDTO();
+    public ProductResponseDTO selectOneProduct(String productCategory,String productTitle) {
+        return productRepository.findByProductCategoryAndProductTitle(productCategory,productTitle).get().toDTO();
     }
 
     @Transactional(rollbackFor = RuntimeException.class)
     @Override
-    public List<ProductResponseDTO> selectAllProduct() {
-        return productRepository.findAll().stream()
-                .map(Product::toDTO)
-                .collect(Collectors.toList());
+    public Page<Product> selectAllProduct(String productCategory, Pageable pageable) {
+        return productRepository.findAllByProductCategory(productCategory, pageable);
+    }
+
+    @Override
+    public List<ProductResponseDTO> selectLimitProduct(String productCateogry) {
+        return (List<ProductResponseDTO>) productRepository.findTop4ByProductCategoryOrderByIdDesc(productCateogry);
     }
 
 
@@ -61,5 +65,8 @@ public class ProductServiceImpl  implements ProductService{
 
 
     }
+
+
+
 
 }
