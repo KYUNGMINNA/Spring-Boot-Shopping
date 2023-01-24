f4e4ed45a63e7c4e32de02316a0b74dbed831f51
KYUNGMIN <nkm4095@naver.com>
2023-01-24 22:26:06 +0900

    # OrderRepository

--- src/main/java/com/sale/shopping/repository/ProductOrderRepository.java
+++ src/main/java/com/sale/shopping/repository/ProductOrderRepository.java
@@ -4,5 +4,9 @@ package com.sale.shopping.repository;
 import com.sale.shopping.model.entity.ProductOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ProductOrderRepository extends JpaRepository<ProductOrder,Integer> {
+    Optional<ProductOrder> findByProductOrderNumber(String productOrderNumber);
+
 }
