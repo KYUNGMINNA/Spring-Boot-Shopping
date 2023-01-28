c10f1322366f196c34659cc3c40a0f26172121d1
KYUNGMIN <nkm4095@naver.com>
2023-01-28 23:18:04 +0900

    # MySQL  기본키 초기화 전략

--- src/main/resources/db/tableInit.sql
+++ src/main/resources/db/tableInit.sql
@@ -1,4 +1,11 @@
-drop table if exists Product;
+SET foreign_key_checks = 0;    # 외래키 체크 설정 해제
+drop table if exists Product cascade;
+drop table if exists ProductOrder cascade;
+SET foreign_key_checks = 1;    # 외래키 체크 설정
+
+
+
+
 create table Product (
                          id integer not null auto_increment,
                          productContent varchar(255) not null,
@@ -7,4 +14,14 @@ create table Product (
                          productPrice integer not null,
                          productTitle varchar(255) not null,
                          primary key (id)
-)
+);
+
+create table if not exists ProductOrder
+(
+    id         int auto_increment
+    primary key,
+    orderCount int not null,
+    productId  int null,
+    constraint FK3svc5ffdaomqsfl8usmdvf1nq
+    foreign key (productId) references shopping.Product (id)
+);
