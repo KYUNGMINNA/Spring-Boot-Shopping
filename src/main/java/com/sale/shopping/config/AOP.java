c76c821d7d2bb3087f2e924b3de3e748eefb2e99
KYUNGMIN <nkm4095@naver.com>
2023-01-29 21:38:16 +0900

    # AOP 설정

--- src/main/java/com/sale/shopping/config/AOP.java
+++ src/main/java/com/sale/shopping/config/AOP.java
@@ -1,13 +1,20 @@
 package com.sale.shopping.config;
 
+import com.sale.shopping.model.dto.CommonDTO;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 
 @Component
 @Aspect
@@ -17,6 +24,33 @@ public class AOP {
 
     @Around("execution(* com.sale.shopping..*.*(..))")
     public Object timeCheck(ProceedingJoinPoint proceedingJoinPoint){
+        String type = proceedingJoinPoint.getSignature().getDeclaringTypeName();
+        String method = proceedingJoinPoint.getSignature().getName();
+
+        Object[] args = proceedingJoinPoint.getArgs();
+
+        for (Object arg : args) {
+            if(arg instanceof BindingResult) {
+                BindingResult bindingResult = (BindingResult) arg;
+
+                // 서비스 : 정상적인 화면 -> 사용자요청
+                Map<String, String> errorMap = null;
+                if (bindingResult.hasErrors()) {
+                    errorMap = new HashMap<>();
+
+                    for (FieldError error : bindingResult.getFieldErrors()) {
+                        errorMap.put(error.getField(), error.getDefaultMessage());
+                        // 로그 레벨 error, warn, info, debug
+                        log.warn(type + "." + method + "() => 필드 : " + error.getField() + ", 메시지 : " + error.getDefaultMessage());
+                        log.debug(type + "." + method + "() => 필드 : " + error.getField() + ", 메시지 : " + error.getDefaultMessage());
+
+
+                    }
+
+                }
+                return new CommonDTO<>(HttpStatus.BAD_REQUEST.value(), errorMap);
+            }
+        }
         long start=System.currentTimeMillis();
 
         log.info("실향 클래스  :  "+proceedingJoinPoint.getTarget());
